76b1f7e050c5ff0535d73d49936091cfdbe7173e
codelapp <info@codelapp.com>
2018-04-17 18:25:01 +0200

    Primer commit

--- FotosCoreData/ViewController.swift
+++ FotosCoreData/ViewController.swift
@@ -1,25 +1,53 @@
-//
-//  ViewController.swift
-//  FotosCoreData
-//
-//  Created by jesus on 17/4/18.
-//  Copyright © 2018 jesus. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var txtNombre: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        txtNombre.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnGuardar(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func btnCamara(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnGaleria(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let imagenTomada = info[UIImagePickerControllerEditedImage] as? UIImage
+        imagen.image = imagenTomada
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+     // Tecla return pulsada
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+     // Cuando se toca en la vista
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
